08a796b5cf9e45f633101e6407f7f8280584dfcb
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-29 23:52:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [1,4,7],
+  [0,3,6],
+  [2,5,8],
+  [6,4,2],
+  [0,4,8],
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do | combination |
+    if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+      return combination # return the win_combination indexes that won.
+    elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      return combination
+    end
+  end
+  else
+     false
+end
+
+def full(board)
+  if board.include?(" ")
+   false
+ else
+   true
+  end
+end
