49e1419b8e5cbfad84aab0bf0072ddbce3f78f1d
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-30 00:37:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,6 +60,7 @@ def winner(board)
       elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
         return "O"
       end
+    end
   else
     return nil
   end
